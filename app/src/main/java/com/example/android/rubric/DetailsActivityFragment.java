900045f305c459b0f8ee3d7d73dfcf92616b1db7
Ivan Jovanovic <ksajvan@gmail.com>
2016-04-12 15:09:48 +0200

    Adding Contract class for sqlite database

--- app/src/main/java/com/example/android/rubric/DetailsActivityFragment.java
+++ app/src/main/java/com/example/android/rubric/DetailsActivityFragment.java
@@ -3,17 +3,30 @@ package com.example.android.rubric;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailsActivityFragment extends Fragment {
+    private static final String LOG_TAG = DetailsActivityFragment.class.getSimpleName();
+    private static final String MOVIES_SHARE_HASHTAG = " #RubricApp";
+    private String mMoviePosterPath;
 
     public DetailsActivityFragment() {
+        // add menu to fragment - this has to be done in order to show up options menu
+        //and also we have to add onCreateOptionsMenu
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -23,11 +36,42 @@ public class DetailsActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String moviePosterPath = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mMoviePosterPath = intent.getStringExtra(Intent.EXTRA_TEXT);
             ((TextView) rootView.findViewById(R.id.detail_text))
-                    .setText(moviePosterPath);
+                    .setText(mMoviePosterPath);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // inflate xml menu added earlier
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        // find share item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // getting the shareActionProvider
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mMoviePosterPath + MOVIES_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
